2ca3142bc72eaea9bac947c5d8d7ec10d810ff4f
Kristian Larsen <klarsen@netflix.com>
2022-06-08 09:47:59 +0800

    added workflows and env variables

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,16 +5,17 @@ defineProps({
     required: true,
   },
 });
+const appName = import.meta.env.VITE_APP_NAME;
+const appDescription = import.meta.env.VITE_APP_DESCRIPTION;
+const gaID = import.meta.env.VITE_GA_ID;
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
-    </h3>
+    <h3>App Name: {{ appName }}</h3>
+    <h3>App Description: {{ appDescription }}</h3>
+    <h3>GA ID: {{ gaID }}</h3>
   </div>
 </template>
 
